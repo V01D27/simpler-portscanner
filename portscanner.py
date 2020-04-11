e0ad5b170c6e5bef7d5e86c65afc8856cd050488
V01D <61503876+V01D27@users.noreply.github.com>
2020-04-11 22:06:30 +0200

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,6 +1,6 @@
 import socket
 #
-def istoffen(ip,port):
+def istoffen(ip, port):
    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    try:
       s.connect((ip, int(port)))
@@ -8,14 +8,16 @@ def istoffen(ip,port):
       print("port", ran,"Offen")
    except:
       print("port", ran,"Zu")
-   
-#ip 
+#ip und port von User
 ip = input("Ip: ")
+
 #für die for schleife
 von = (int(input("Von: ")))
 bis = (int(input("Bis: ")))
 bis = ((bis) + 1)
 #for schleife
+print("*" * 50)
 for ran in range((von), (bis)):
     # print(port)
     istoffen(ip, ran)
+print("*" * 50)
